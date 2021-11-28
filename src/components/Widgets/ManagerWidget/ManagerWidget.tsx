1db0f336f0d68d84d87cefefa17f81cbbe8f24c1
Vyacheslav Glushko <vyacheslav.glushko@masterdelivery.ru>
2021-11-28 15:07:03 +0300

    manager widget

--- src/components/Widgets/ManagerWidget/ManagerWidget.tsx
+++ src/components/Widgets/ManagerWidget/ManagerWidget.tsx
@@ -1,14 +1,63 @@
 import './ManagerWidget.scss';
 
+import {
+    IUser,
+    UserContactTypes,
+} from 'src/index.types';
+
 import * as b_ from 'b_';
 import React from 'react';
 
+import Avatar from 'src/components/Avatar/Avatar';
+import Button, {
+    ButtonVariants,
+} from 'src/components/Button/Button';
+
 const b = b_.with('manager-widget');
 
-export default function ManagerWidget() {
+interface IProps {
+    user: IUser;
+}
+
+const CONTACTS_BUTTON_TEXT = {
+    [UserContactTypes.Slack]: 'Slack',
+    [UserContactTypes.Email]: 'Email',
+};
+
+export default function ManagerWidget({
+    user,
+}: IProps) {
     return (
         <div className={b()}>
-
+            <div className={b('title')}>
+                Account Manager
+            </div>
+            <div className={b('user-meta')}>
+                <div className={b('user-avatar')}>
+                    <Avatar
+                        avatar={user.avatar}
+                        size={'m'}
+                    />
+                </div>
+                <div className={b('user-name')}>
+                    {user.name}
+                </div>
+            </div>
+            <div className={b('user-contacts')}>
+                {user.contacts.map(contact => (
+                    <div
+                        key={contact.type}
+                        className={b('user-contact-item')}
+                    >
+                        <Button
+                            size={'s'}
+                            variant={ButtonVariants.Action}
+                        >
+                            {CONTACTS_BUTTON_TEXT[contact.type]}
+                        </Button>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
